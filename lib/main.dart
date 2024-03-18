2b57247ab4eb2b74d4c0c4e90b8357b54f608e33
David Pulgarin <davidbohorquez150@gmail.com>
2024-03-17 23:43:47 -0500

    Inicio: Inicializacion del proyectos app ToDo

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const data = 'HOLA MUNDOO';
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(data),
+          
+        ),
+      ),
+    );
+  }
+}
