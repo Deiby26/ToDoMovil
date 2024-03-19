629aef39ccd9e0ffa03dc3a74f7d8aa8e0785507
David Pulgarin <davidbohorquez150@gmail.com>
2024-03-18 20:45:25 -0500

    Update: Actualizacion de cambios en el proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,77 @@
 import 'package:flutter/material.dart';
-import 'controller/functions.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
+class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    const data = 'UNIVERSIDAD DE MEDELLIN!';
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Lista de Tareas"),),
-        body: Column(
-          children: [
-            ListView(
-              shrinkWrap: true,
-              children: const [
-                Text("Tarea 1 "),
-                Text("Tarea 2 "),
-                Text("Tarea 3")],
-            ),
-             TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.green),
-              
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const Formulario()),
-                );
-
-              },
-               child: 
-               const Text("Agregar", style: TextStyle(color: Colors.white)))
-          
-          ],
-        )
-        
-      ),
+      home: MyHomePage(),
     );
   }
 }
 
-
-
-class Formulario extends StatelessWidget {
-  const Formulario({super.key});
-
+class MyHomePage extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    String data = "holamundo";
+    
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text(data),
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Lista de tareas'),
+      ),
+      body: Column(
+        children: [
+           ListTile(
+            title:  const Text("Lista de pendientes"),
+            subtitle: const Text("Universidad de Medellin"),
+            onTap: () {
+              print("HOLAMUNDO");
+            },
+            
+
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Nombre de la tarea'),
+                    content: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        labelText: 'Ingrese el nombre de la tarea',
+                        border: OutlineInputBorder(),
+                      ),
+
+                    ),
+                    //Aqui tengo que construir lo de la descripcion 
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          String textoIngresado = _controller.text;
+                          Navigator.of(context).pop();
+                          _controller.clear();
+                          print(textoIngresado);
+                        },
+                        child: const Text('Aceptar'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: const Text('Agregar'),
+          ),
+          //const Text("universidad de medellin")
+        ],
       ),
     );
   }
 }
-
-
-
