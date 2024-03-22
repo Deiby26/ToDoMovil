b5ee9e8298a9f9f023704310d32a1e4c0e903e4f
David Pulgarin <davidbohorquez150@gmail.com>
2024-03-22 11:31:09 -0500

    Update: Se agrego la lista de Wigets

--- lib/main.dart
+++ lib/main.dart
@@ -24,59 +24,116 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _controller2 = TextEditingController();
   String username_ = '';
   String descrip = '';
-  bool? ischeked_ = false;
+  String aletarVacio = '';
+  bool isChecked_ = false;
+  bool ischeked2 = false;
+
+  List<Widget> contenedor = [];
+  int _widgetCount = 0;
+
+  void eliminarElemento(Key key) {
+    setState(() {
+      contenedor.removeWhere((widget) => widget.key == key);
+    });
+  }
+
+  void addtask() {
+    setState(() {
+      _widgetCount++;
+      Key newKey = UniqueKey();
+      contenedor.add(
+        ListTile(
+          key: newKey,
+          title: Text(username_),
+          subtitle: Text(descrip),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text("Estado de la tarea"),
+                  content: StatefulBuilder(
+                    builder: (BuildContext context, StateSetter setState) {
+                      return Column(
+                        children: [
+                          CheckboxListTile(
+                            title: const Text("Tarea completada"),
+                            value: isChecked_,
+                            onChanged: (bool? newValue) {
+                              setState(() {
+                                isChecked_ = newValue!;
+                              });
+                            },
+                          ),
+                          CheckboxListTile(
+                            value: ischeked2,
+                            title: const Text('Tarea incompleta'),
+                            onChanged: (bool? newValue2) {
+                              setState(() {
+                                ischeked2 = newValue2!;
+                              });
+                            },
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.red,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              eliminarElemento(newKey);
+                            },
+                            child: const Text(
+                              "Eliminar tarea",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        isChecked_ = false;
+                        ischeked2 = false;
+                      },
+                      child: const Text('Cerrar'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          if (isChecked_ == true && ischeked2 == true) {
+                          } else if (isChecked_ == true) {
+                            Navigator.of(context).pop();
+                          } else if (ischeked2 == true) {
+                            Navigator.of(context).pop();
+                          }
+                        });
+                      },
+                      child: const Text("Aceptar"),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Lista de tareas'),
+        backgroundColor: Colors.grey,
+        title: const Text('Lista de Tareas'),
       ),
       body: Column(
         children: [
-          ListTile(
-            title: Text(username_),
-            subtitle: Text(descrip),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text("Estado de la tarea"),
-                    content: Column(
-                      children: [
-                        CheckboxListTile(
-                            title: const Text("Tarea completada"),
-                            value: ischeked_,
-                            onChanged: (bool? newValue) {
-                              setState(() {
-                                ischeked_ = newValue;
-                              });
-                            }),
-                        TextButton(
-                          style:
-                              TextButton.styleFrom(backgroundColor: Colors.red),
-                          onPressed: () {},
-                          child: const Text(
-                            "Eliminar tarea",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Cerrar'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
+          Column(
+            children: contenedor,
           ),
           ElevatedButton(
             onPressed: () {
@@ -88,19 +145,22 @@ class _MyHomePageState extends State<MyHomePage> {
                     content: Column(
                       children: [
                         const Text("NOMBRE"),
-                        TextField(
-                            controller: _controller,
-                            decoration: const InputDecoration(
-                              labelText: 'Nombre corto',
-                              border: OutlineInputBorder(),
-                            )),
+                        TextFormField(
+                          controller: _controller,
+                          decoration: const InputDecoration(
+                            labelText: 'Nombre corto',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
                         const Text("DESCRIPCION"),
                         TextField(
                           controller: _controller2,
                           decoration: const InputDecoration(
-                              labelText: "Pequeña descripcion",
-                              border: OutlineInputBorder()),
-                        )
+                            labelText: "Pequeña descripcion",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        Text(aletarVacio),
                       ],
                     ),
                     actions: <Widget>[
@@ -108,23 +168,50 @@ class _MyHomePageState extends State<MyHomePage> {
                         onPressed: () {
                           String textoNombre = _controller.text;
                           String textoDescripcion = _controller2.text;
-                          Navigator.of(context).pop();
                           _controller.clear();
                           _controller2.clear();
-                          print(textoNombre);
-                          print(textoDescripcion);
                           setState(() {
-                            username_ = textoNombre;
-                            descrip = textoDescripcion;
+                            if (textoNombre == null || textoNombre.isEmpty) {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Error'),
+                                    content: const Text(
+                                      'El nombre no debe estar vacío',
+                                    ),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text('Cancelar'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            } else {
+                              Navigator.of(context).pop();
+                              username_ = textoNombre;
+                              descrip = textoDescripcion;
+                              addtask();
+                            }
                           });
                         },
                         child: const Text('Aceptar'),
                       ),
                       TextButton(
-                          onPressed: () {
+                        onPressed: () {
+                          setState(() {
+
+
+                            
                             Navigator.of(context).pop();
-                          },
-                          child: const Text("Cancelar"))
+                          });
+                        },
+                        child: const Text("cancel"),
+                      ),
                     ],
                   );
                 },
